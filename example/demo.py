ab9a9c2d6c20b2fdce4569754a7b18287c45af14
leooreo110 <96639609+teannafeng@users.noreply.github.com>
2025-10-01 14:52:30 -0700

    Update demo.py

--- example/demo.py
+++ example/demo.py
@@ -21,22 +21,32 @@ lasso_res = mod.run(y, X, cv_folds=10)
 
 # %%
 # print lasso results
+
+print("\n>>> Print lasso results.")
 print_lasso(lasso_res, true_betas, digits=2)
 
 # %%
 # run ols regression with active predictors selected by lasso
-run_ols_reg(y, X, subset=[0,1,2], digits=2)
+
+print("\n>>> Run ols regression with active predictors selected by lasso.")
+print(run_ols_reg(y, X, subset=[0,1,2], digits=2))
 
 # %%
 # plot estimated betas and selected lambda
+
+print("\n>>> Plot estimated betas and selected lambda.")
 plot_lasso_ols(lasso_res, y, X, threshold=0.3)
 
 # %%
-# ols regression with all predictors
-run_ols_reg(y, X, digits=2)
+# run ols regression with all predictors
+
+print("\n>>> Run ols regression with all predictors.")
+print(run_ols_reg(y, X, digits=2))
 
 # %% 
-# lambda = 0 + no cv --> ols regression with all predictors
+# run with lambda = 0 + no cv --> ols regression with all predictors
+
+print("\n>>> Run with lambda = 0 + no cv --> ols regression with all predictors.")
 res_ = mod.run(y, X, lams=[0], cv_folds=0, max_iter=100)
 print_lasso(res_, true_betas, digits=2)
 
